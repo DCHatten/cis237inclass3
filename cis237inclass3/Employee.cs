1b0eebe1d8f1fa009003492959c592879f755ad9
David Hatten <dhatten87@gmail.com>
2016-09-29 16:50:35 -0400

    Added a parent class for employees and a child class for salaried employees

---
+++ cis237inclass3/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass3
+{
+    class Employee
+    {
+        //Backing fields
+        protected string _firstName;
+        protected string _lastName;
+        protected DateTime _startDate;
+        //Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+        //Methods
+        public override string ToString()
+        {
+            return _firstName + " " + _lastName;
+        }
+        public DayOfWeek GetStartDay()
+        {
+            return _startDate.DayOfWeek;
+        }
+        //Constructors
+        public Employee(string FirstName, string LastName, DateTime StartDate)
+        {
+            _firstName = FirstName;
+            _lastName = LastName;
+            _startDate = StartDate;
+        }
+        public Employee()
+        {
+            //Default Constructor
+        }
+    }
+}
---
+++ cis237inclass3/SalaryEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass3
+{
+    class SalaryEmployee : Employee
+    {
+        //Backing Fields
+        private decimal _weeklySalary;
+        //Properties
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+        //Methods
+        public string NameAndSalary()
+        {
+            return base.ToString() + " " + _weeklySalary.ToString("C");
+        }
+        //Constructor
+        public SalaryEmployee(string FirstName, string LastName, DateTime StartDate, 
+            decimal WeeklySalary) : base(FirstName,LastName,StartDate)
+        {
+            _weeklySalary = WeeklySalary;
+        }
+        public SalaryEmployee() : base()
+        {
+            //Default Constructor
+        }
+    }
+}
